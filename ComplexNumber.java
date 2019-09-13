d06e57afffaf7c5236356cdcc533f0ff1b8abf6e
RockyWasTaken <55099738+RockyWasTaken@users.noreply.github.com>
2019-09-13 12:59:37 -0400

    Latest: Uses double for instance variables

--- ComplexNumber.java
+++ ComplexNumber.java
@@ -1,50 +1,48 @@
 public class ComplexNumber<Public> {
 
-    private Fraction real;
-    private Fraction imaginary;
+    private double real;
+    private double imaginary;
 
     // constructor takes the real and imaginary parts of the number and sets up the complex number
-    public ComplexNumber(Fraction real, Fraction imaginary){
+    public ComplexNumber(double real, double imaginary){
         this.real = real;
         this.imaginary = imaginary;
     }
 
     // constructor takes a real number and sets up the complex number
-    public ComplexNumber(Fraction number){
+    public ComplexNumber(double number){
         this.real = number;
-        this.imaginary = new Fraction(0, 1);
+        this.imaginary = 0;
     }
 
     // constructor takes a string representation of a complex number, say “1+2i”, and sets up the fraction
     public ComplexNumber(String complexNumber){
         int plusSignIndex = complexNumber.indexOf("+");
         int iIndex = complexNumber.indexOf("i");
-        this.real = new Fraction(Integer.valueOf(complexNumber.substring(0, plusSignIndex)));
-        this.imaginary = new Fraction(Integer.valueOf(complexNumber.substring(plusSignIndex + 1, iIndex)));
+        this.real = Double.valueOf(complexNumber.substring(0, plusSignIndex));
+        this.imaginary = Double.valueOf(complexNumber.substring(plusSignIndex + 1, iIndex));
     }
 
 
     // adds complex number c to this complex number - returns the resulting complex number
     public ComplexNumber add(ComplexNumber c){
-        return new ComplexNumber(this.real.add(c.real), this.imaginary.add(c.imaginary));
+        return new ComplexNumber(this.real + c.real, this.imaginary + c.imaginary);
     }
 
     // subtracts complex number c from this complex number - returns the resulting complex number
     public ComplexNumber subtract(ComplexNumber c){
-        return new ComplexNumber(this.real.subtract(c.real), this.imaginary.subtract(c.imaginary));
+        return new ComplexNumber(this.real - c.real, this.imaginary - c.imaginary);
     }
 
     // multiplies complex number c by this complex number -  returns the resulting complex number
     public ComplexNumber multiply(ComplexNumber c){
-        return new ComplexNumber(((this.real.multiply(c.real)).subtract((this.imaginary.multiply(c.imaginary)))),
-                ((this.real.multiply(c.imaginary)).add((this.imaginary.multiply(c.real)))));
+        return new ComplexNumber(((this.real * c.real) - (this.imaginary * c.imaginary)),
+                ((this.real * c.imaginary) + (this.imaginary * c.real)));
     }
 
     // divides complex number c to this complex number - returns the resulting complex number
     public ComplexNumber divide(ComplexNumber c){
-        return new ComplexNumber(
-                new Fraction((this.real.multiply(c.real).add(this.imaginary.multiply(c.imaginary))), (c.real.multiply(c.real).add(c.imaginary.multiply(c.imaginary)))),
-                new Fraction((this.imaginary.multiply(c.real).subtract(this.real.multiply(c.imaginary))), (c.real.multiply(c.real).add(c.imaginary.multiply(c.imaginary))));
+        return new ComplexNumber(((this.real * c.real) + (this.imaginary + c.imaginary)) / ((c.real * c.real) + (c.imaginary * c.imaginary)), ((this.imaginary * c.real) - (this.real * c.imaginary) / ((c.real * c.real) + (c.imaginary * c.imaginary))));
     }
 
     // compares this complex number to another complex number
@@ -52,17 +50,17 @@ public class ComplexNumber<Public> {
     // return 0 if the complex numbers are equal
     // return a positive number if this complex number is greater than the other
     public int compareTo(ComplexNumber c){
-        if(this.real.compareTo(c.real) < 0){
+        if(this.real < c.real){
             return -1;
         }
-        else if(this.real.compareTo(c.real) > 0){
+        else if(this.real > c.real){
             return 1;
         }
-        else if(this.real.equals(c.real)){
-            if(this.imaginary.compareTo(c.imaginary) < 0){
+        else if(this.real == c.real){
+            if(this.imaginary < c.imaginary){
                 return -1;
             }
-            else if(this.imaginary.compareTo(c.imaginary) > 0){
+            else if(this.imaginary > c.imaginary){
                 return 1;
             }
         }
