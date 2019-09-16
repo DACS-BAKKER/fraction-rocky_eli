72e7923ef75eed6a9fc957a6fdf8976a121093cd
powerfulTaxi <37839848+powerfulTaxi@users.noreply.github.com>
2019-09-15 20:27:01 -0400

    Latest version

--- Calculator.java
+++ Calculator.java
@@ -6,6 +6,9 @@
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -18,38 +21,52 @@ public class Calculator implements ActionListener {
 
             JFrame f= new JFrame();
 
+            JLabel title = new JLabel("Calculator");
+            title.setBounds(75,20,100,15);
+            title.setHorizontalAlignment(JLabel.CENTER);
+
             tFrac1 = new JTextField();
-            tFrac1.setText("Enter Fraction 1");
+            tFrac1.setText("Enter Value 1");
             tFrac1.setHorizontalAlignment(JTextField.CENTER);
-            tFrac1.setBounds(95,50,150,20);
+            tFrac1.setBounds(50,50,150,20);
 
             tFrac2 = new JTextField();
-            tFrac2.setText("Enter Fraction 2");
+            tFrac2.setText("Enter Value 2");
             tFrac2.setHorizontalAlignment(JTextField.CENTER);
-            tFrac2.setBounds(95,100,150,20);
+            tFrac2.setBounds(50,100,150,20);
 
             tResult = new JTextField();
             tResult.setText("Result");
             tResult.setHorizontalAlignment(JTextField.CENTER);
-            tResult.setBounds(95,150,150,20);
+            tResult.setBounds(50,150,150,20);
             tResult.setEditable(false);
 
             bAdd = new JButton("+");
-            bAdd.setBounds(40,200,50,50);
+            bAdd.setBounds(30,200,40,40);
+
             bSubtract = new JButton("-");
-            bSubtract.setBounds(110,200,50,50);
+            bSubtract.setBounds(80,200,40,40);
+
             bMultiply = new JButton("*");
-            bMultiply.setBounds(180,200,50,50);
+            bMultiply.setBounds(130,200,40,40);
+
             bDivide = new JButton("/");
-            bDivide.setBounds(250,200,50,50);
+            bDivide.setBounds(180,200,40,40);
 
             bAdd.addActionListener(this);
             bSubtract.addActionListener(this);
             bMultiply.addActionListener(this);
             bDivide.addActionListener(this);
 
-            f.add(tFrac1);f.add(tFrac2);f.add(tResult);f.add(bAdd);f.add(bSubtract);f.add(bMultiply);f.add(bDivide);
-            f.setSize(340,300);
+
+            JTextArea log = new JTextArea();
+            log.setBounds(250,10,240,260);
+            log.setEditable(false);
+            JScrollPane scroll = new JScrollPane(log, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+
+
+            f.add(tFrac1);f.add(tFrac2);f.add(tResult);f.add(bAdd);f.add(bSubtract);f.add(bMultiply);f.add(bDivide);f.add(title);f.add(log);f.add(scroll);
+            f.setSize(500,300);
             f.setLayout(null);
             f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             f.setVisible(true);
